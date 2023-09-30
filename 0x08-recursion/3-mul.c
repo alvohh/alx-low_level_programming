05e9cd9e7de35643cf967bc1606f6083b425d70c
alvohh <alvomukiri@gmail.com>
2023-09-29 18:34:03 -0700

    task 4

---
+++ 0x08-recursion/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints multiplication
+*of two numbers
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: returns 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, j, mul;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
